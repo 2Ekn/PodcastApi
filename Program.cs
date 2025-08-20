8f0c72e59dbd321825b652d2e3995c798eab8adc
2Ekn <johannes.ekendahl@hotmail.com>
2025-08-20 15:53:56 +0200

    .

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PodcastApi.Data;
 using PodcastApi.Interfaces;
 using PodcastApi.Services;
@@ -49,6 +50,29 @@ namespace PodcastApi
             builder.Services.AddAuthorization();
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Podcast API", Version = "v1" });
+
+                var jwtScheme = new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter: Bearer {your token}"
+                };
+
+                c.AddSecurityDefinition("Bearer", jwtScheme);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        { jwtScheme, Array.Empty<string>() }
+                    });
+                });
 
             //Registration of created services
             builder.Services.AddScoped<IAuthService, AuthService>();
@@ -56,10 +80,13 @@ namespace PodcastApi
 
             var app = builder.Build();
 
+
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();                
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
